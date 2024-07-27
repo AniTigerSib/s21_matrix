15be7f4428cbf0272557070a4dd4dc7fee73b073
Michael <khudoleev04@mail.ru>
2024-07-28 01:29:07 +0500

    matrix/Remove function updated, added creation function

--- src/martix/s21_create_matrix.c
+++ src/martix/s21_create_matrix.c
@@ -1,5 +1,31 @@
 #include "s21_matrix.h"
 
 int s21_create_matrix(int rows, int columns, matrix_t *result) {
-  
+  if (result == NULL || rows < 1 || columns < 1 || result->matrix != NULL) {
+    return ERROR;
+  }
+
+  int code = 0;
+
+  result->rows = rows;
+  result->columns = columns;
+
+  double **temp_matrix = (double **)calloc(rows, sizeof(double *));
+  code = (temp_matrix == NULL) * ERROR;
+  if (!code) result->matrix = temp_matrix;
+
+  if (!code) {
+    double *temp = NULL;
+    for (int row = 0; !code && row < rows; row++) {
+      temp = (double *)calloc(columns, sizeof(double));
+      code = (temp == NULL) * ERROR;
+      if (!code) result->matrix[row] = temp;
+    }
+  }
+
+  if (code && result->matrix) {
+    s21_remove_matrix(result);
+  }
+
+  return code;
 }
--- src/martix/s21_remove_matrix.c
+++ src/martix/s21_remove_matrix.c
@@ -1,15 +1,24 @@
 #include "s21_matrix.h"
 
 void s21_remove_matrix(matrix_t *A) {
+  int rows = 0;
   int code = (A == NULL);
+
   if (!code) {
-    for (int i = 0; i < A->rows; i++) {
+    rows = A->rows;
+    A->columns = 0;
+    A->rows = 0;
+    code = (A->matrix == NULL);
+  }
+
+  if (!code) {
+    for (int i = 0; i < rows; i++) {
       if (A->matrix[i] != NULL) {
         free(A->matrix[i]);
         A->matrix[i] = NULL;
       }
     }
-    A->columns = 0;
-    A->rows = 0;
+    free(A->matrix);
+    A->matrix = NULL;
   }
 }
