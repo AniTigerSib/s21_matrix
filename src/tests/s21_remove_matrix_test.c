8f6570ec6846db2d0bac9787d1b9ace4c8611c73
Michael <khudoleev04@mail.ru>
2024-07-27 18:01:22 +0500

    tests/Tests for removing function updated

--- src/tests/s21_remove_matrix_test.c
+++ src/tests/s21_remove_matrix_test.c
@@ -9,6 +9,7 @@ START_TEST(remove_matrix_normal)
   s21_remove_matrix(matrix);
   ck_assert_ptr_null(matrix);
 }
+END_TEST
 
 START_TEST(remove_matrix_null_pointer)
 {
@@ -16,6 +17,33 @@ START_TEST(remove_matrix_null_pointer)
   s21_remove_matrix(matrix);
   ck_assert_ptr_null(matrix);
 }
+END_TEST
+
+START_TEST(remove_matrix_null_pointer_inside)
+{
+  matrix_t *matrix = NULL;
+  s21_create_matrix(3, 3, matrix);
+  for (int i = 0; i < 3; i++) {
+    free(matrix->matrix[i]);
+    matrix->matrix[i] = NULL;
+  }
+  free(matrix->matrix);
+  matrix->matrix = NULL;
+  s21_remove_matrix(matrix);
+  ck_assert_ptr_null(matrix);
+}
+END_TEST
+
+START_TEST(remove_matrix_null_pointer_inside_normal_pointer)
+{
+  matrix_t *matrix = NULL;
+  s21_create_matrix(3, 3, matrix);
+  free(matrix->matrix[1]);
+  matrix->matrix[1] = NULL;
+  s21_remove_matrix(matrix);
+  ck_assert_ptr_null(matrix);
+}
+END_TEST
 
 Suite *s21_remove_matrix_suite(void) {
   Suite *s = suite_create("s21_remove_matrix_test");
@@ -23,6 +51,8 @@ Suite *s21_remove_matrix_suite(void) {
 
   tcase_add_test(tc_core, remove_matrix_normal);
   tcase_add_test(tc_core, remove_matrix_null_pointer);
+  tcase_add_test(tc_core, remove_matrix_null_pointer_inside);
+  tcase_add_test(tc_core, remove_matrix_null_pointer_inside_normal_pointer);
 
   suite_add_tcase(s, tc_core);
   return s;
