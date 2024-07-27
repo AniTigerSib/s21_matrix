7c90aed48fb009bb1de13949126290bfa077dd48
Michael <khudoleev04@mail.ru>
2024-07-27 23:58:39 +0500

    matrix/Updated matrix remove function, needed to test

--- src/martix/s21_remove_matrix.c
+++ src/martix/s21_remove_matrix.c
@@ -1,5 +1,14 @@
 #include "s21_matrix.h"
 
 void s21_remove_matrix(matrix_t *A) {
-  
+  int code = (A == NULL);
+  if (!code) {
+    for (int i = 0; i < A->rows; i++) {
+      if (A->matrix[i] != NULL) {
+        free(A->matrix[i]);
+        A->matrix[i] = NULL;
+      }
+    }
+    free(A);
+  }
 }
